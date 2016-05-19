12a5abe0a1b276e29f31a4506eb9e3e472b8a38f
Jenna Montero and Zach Holz <jenna@>
2016-05-18 20:42:30 -0700

    Create sloth website with starter jQuery calls on three divs

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+  $(".peekaboo-action").click(function() {
+    $("#peekaboo-showing").slideToggle();
+    $("#peekaboo-hidden").slideToggle();
+  });
+});
+
+$(function() {
+  $(".love-action").click(function() {
+    $("#lovesloths-showing").slideToggle();
+    $("#lovesloths-hidden").slideToggle();
+  });
+});
+
+$(function() {
+  $(".sleep-action").click(function() {
+    $("#sleepingsloths-showing").slideToggle();
+    $("#sleepingsloths-hidden").slideToggle();
+  });
+});
